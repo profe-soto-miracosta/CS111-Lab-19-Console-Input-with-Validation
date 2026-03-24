418ab2169c177dccdab7aa3f8550a94182b1f8c7
Raul Soto <rsoto@miracosta.edu>
2026-03-24 13:43:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
+/**
+ * Your program description here
+ *
+ * @author Your name here
+ */
 public class Main
 {
 	public static void main(String[] args)
 	{
-		
-
+		// TODO: Create an array of `NetflixOriginal` shows of size 2. Instantiate two NetflixOriginal objects by calling the instantiateFromInput() method.
 	}
 
+	// TODO: Create instantiateFromInput() method.
 }
